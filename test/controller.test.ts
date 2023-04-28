67f190a646f824ba136b6250686b49fa9dfe9cf2
Piccio98 <104207695+Piccio98@users.noreply.github.com>
2023-04-28 11:54:40 +0200

    test: added test components

---
+++ test/controller.test.ts
@@ -0,0 +1,149 @@
+import { beforeAll, describe, expect, it, vi } from "vitest";
+import { createPreference, updatePreference, getPreferenceById, getAvatarById} from "../db/controller/controller";
+import { ObjectId } from "mongodb";
+import { collections, connectToDatabase } from "../db/services/database.service";
+
+
+interface Response {
+    status: number | any
+    json: any
+}
+
+describe('Activity controller', async () => {
+    beforeAll(async () => {
+        await connectToDatabase()
+        vi.clearAllMocks();
+    });
+    const mockRequest = (id: ObjectId) => ({ params: { id } });
+    const mockResponse = () => {
+        const res: Response = {
+            json: {},
+            status: {}
+        };
+        res.status = vi.fn().mockReturnValue(res);
+        res.json = vi.fn().mockReturnValue(res);
+        return res;
+    };
+    const mockCollections = {
+        activity: {
+            findOne: vi.fn(),
+        },
+    };
+
+    beforeAll(() => {
+        vi.clearAllMocks();
+    });
+
+    it('should return ok for existing preference', async () => {
+        const req = {
+            params: {
+                id: "62bee981e63f093c813b8a02"
+            }
+        };
+        const res = mockResponse();
+        await getPreferenceById(req, res);
+        expect(res.status).toHaveBeenCalledWith(200);
+    });
+
+    it('should return error getting for no preference id', async () => {
+        const req = {
+            params: {
+                id: "111111111111"
+            }
+        };
+        const res = mockResponse();
+        await getPreferenceById(req, res);
+        expect(res.status).toHaveBeenCalledWith(404);
+    });
+
+    it('should return error creating for no preference id', async () => {
+        const req = {
+            params: {
+                id: ""
+            }
+        };
+        const res = mockResponse();
+        await createPreference(req, res);
+        expect(res.status).toHaveBeenCalledWith(400);
+    });
+
+    it('should return ok creating new preference', async () => {
+        const req = {
+            params: {
+                id: "111111111111"
+            }
+        };
+        const res = mockResponse();
+        await createPreference(req, res);
+        expect(res.status).toHaveBeenCalledWith(200);
+    });
+
+    it('should return error updating for no preference id', async () => {
+        const req = {
+            params: {
+                id: ""
+            }
+        };
+        const res = mockResponse();
+        await updatePreference(req, res);
+        expect(res.status).toHaveBeenCalledWith(400);
+    });
+
+    it('should return error updating for wrong preference id', async () => {
+        const req = {
+            params: {
+                id: "999999999999"
+            }
+        };
+        const res = mockResponse();
+        await updatePreference(req, res);
+        expect(res.status).toHaveBeenCalledWith(401);
+    });
+
+    it('should return ok updating preference', async () => {
+        const req = {
+            params: {
+                id: "111111111111",
+                notification: false
+            }
+        };
+        const res = mockResponse();
+        await updatePreference(req, res);
+        expect(res.status).toHaveBeenCalledWith(200);
+    });
+
+    it('should return error getting avatar for no preference id', async () => {
+        const req = {
+            params: {
+                id: ""
+            }
+        };
+        const res = mockResponse();
+        await getAvatarById(req, res);
+        expect(res.status).toHaveBeenCalledWith(400);
+    });
+
+    it('should return error getting avatar for wrong preference id', async () => {
+        const req = {
+            params: {
+                id: "999999999999"
+            }
+        };
+        const res = mockResponse();
+        await getAvatarById(req, res);
+        expect(res.status).toHaveBeenCalledWith(401);
+    });
+
+    it('should return ok getting avatar', async () => {
+        const req = {
+            params: {
+                id: "111111111111",
+            }
+        };
+        const res = mockResponse();
+        await getAvatarById(req, res);
+        expect(res.status).toHaveBeenCalledWith(200);
+        await collections.preference?.deleteOne({ userId: new ObjectId("111111111111") });
+    });
+
+});
--- test/index.test.ts
+++ test/index.test.ts
@@ -2,10 +2,15 @@ import { describe, expect, it } from "vitest";
 import app from "../index"
 import request from 'supertest';
 
-describe('GET /', () => {
-    it('should respond with a JSON object containing a message', async () => {
+describe('index test', () => {
+    it('should respond with error creating an activity', async () => {
         const response = await request(app.app).get('/');
-        expect(response.statusCode).toBe(200);
-        expect(response.body).toEqual({ "msg": "test" });
+        expect(response.statusCode).toBe(404);
     });
+
+    it('should respond with error getting an activity', async () => {
+        const response = await request(app.app).get('/111111111111');
+        expect(response.statusCode).toBe(404);
+    });
+
 });
