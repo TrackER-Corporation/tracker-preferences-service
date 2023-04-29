05f8b7c3adc4a33a235c5cc8448b9405b2af67ed
Piccio98 <104207695+Piccio98@users.noreply.github.com>
2023-04-29 13:11:44 +0200

    refactor: update routes

--- db/route/route.ts
+++ db/route/route.ts
@@ -2,9 +2,9 @@ import express from 'express'
 const router = express.Router()
 import { createPreference, updatePreference, getPreferenceById, getAvatarById } from '../controller/controller'
 
-router.post('/api/preference/:id', createPreference)
-router.put('/api/preference/:id', updatePreference)
-router.get('/api/preference/:id', getPreferenceById)
-router.get('/api/preference/avatar/:id', getAvatarById)
+router.post('/:id', createPreference)
+router.put('/:id', updatePreference)
+router.get('/:id', getPreferenceById)
+router.get('/avatar/:id', getAvatarById)
 
 export default router
