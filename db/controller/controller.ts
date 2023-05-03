05360662a2cbe54f79dcf4523282e65abe112f70
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-05-03 15:39:42 +0200

    test: update test

--- db/controller/controller.ts
+++ db/controller/controller.ts
@@ -5,11 +5,11 @@ import { ObjectId } from 'mongodb';
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
-export const getPreferenceById = asyncHandler(async (req, res) => {
+export const getPreferenceById = asyncHandler(async (req, res, next) => {
     const goal = await collections.preference?.findOne({ userId: new ObjectId(req.params.id) })
     if (!goal) {
         res.status(404)
-        throw new Error('Not found')
+        throw Error('Not found')
     }
     res.status(200).json({ goal })
 })
@@ -20,7 +20,7 @@ export const getPreferenceById = asyncHandler(async (req, res) => {
 export const createPreference = asyncHandler(async (req, res) => {
     if (req.params.id === null || !req.params.id) {
         res.status(400)
-        return
+        throw Error('Not found')
     }
     const preference = await collections?.preference?.insertOne({
         userId: new ObjectId(req.params.id),
@@ -40,12 +40,12 @@ export const createPreference = asyncHandler(async (req, res) => {
 export const updatePreference = asyncHandler(async (req, res) => {
     if (!req.params.id) {
         res.status(400)
-        return
+        throw Error('Not found')
     }
     const preference = await collections?.preference?.findOne({ userId: new ObjectId(req.params.id) })
     if (!preference) {
         res.status(401)
-        return
+        throw Error('Not found')
     }
     const update = await collections?.preference?.findOneAndUpdate(
         { userId: new ObjectId(req.params.id) },
@@ -58,12 +58,12 @@ export const updatePreference = asyncHandler(async (req, res) => {
 export const getAvatarById = asyncHandler(async (req, res) => {
     if (!req.params.id) {
         res.status(400)
-        return
+        throw Error('Not found')
     }
     const goal = await collections?.preference?.findOne({ userId: new ObjectId(req.params.id) })
     if (!goal) {
         res.status(401)
-        return
+        throw Error('Not found')
     }
 
     res.status(200).json(goal.avatar)
@@ -72,7 +72,7 @@ export const getAvatarById = asyncHandler(async (req, res) => {
 export const deleteAvatarByUserId = asyncHandler(async (req, res) => {
     if (!req.params.id) {
         res.status(400)
-        return
+        throw Error('Not found')
     }
     const goal = await collections?.preference?.findOne({ userId: new ObjectId(req.params.id) })
     if (goal === null) {
